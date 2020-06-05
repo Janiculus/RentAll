83a1e2a15970b88de42be0f64cc61567156e196b
kirillskibenko <03041999kirill@gmail.com>
2020-06-05 21:08:21 +0300

    product-deteils-css

--- sajt/src/app/app.module.ts
+++ sajt/src/app/app.module.ts
@@ -4,11 +4,27 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { UserComponent } from './user/user.component';
 import { HomePageComponent } from './home-page/home-page.component';
-import {RouterModule} from '@angular/router';
+import {RouterModule, Routes} from '@angular/router';
+import { ViewCategoryModule } from './view-category/view-category.module';
+import { CategoryComponent } from './view-category/category/category.component';
+import { ProductListComponent } from './view-category/category/product-list/product-list.component';
+import { ProductDetailsComponent } from './view-category/category/product-details/product-details.component';
 
-const routes = [
+const productRoutes: Routes = [
+  {path: 'products/:id', component: ProductDetailsComponent},
+  {path: ':id/products/:id', component: ProductDetailsComponent},
+  {path: 'search/:keyword/products/:id', component: ProductDetailsComponent},
+  {path: 'search/:keyword', component: ProductListComponent},
+  {path: ':id', component: ProductListComponent},
+  {path: '', component: ProductListComponent},
+  {path: 'products', component: ProductListComponent},
+  {path: '**', redirectTo: '/category', pathMatch: 'full'}
+]
+
+const routes: Routes = [
   {path: '', component: HomePageComponent},
-  {path: 'user', component: UserComponent}
+  {path: 'user', component: UserComponent},
+  {path: 'category', component: CategoryComponent, children: productRoutes}
 ];
 
 @NgModule({
@@ -19,7 +35,8 @@ const routes = [
   ],
   imports: [
     BrowserModule,
-    RouterModule.forRoot(routes)
+    RouterModule.forRoot(routes),
+    ViewCategoryModule
   ],
   providers: [],
   bootstrap: [AppComponent]
