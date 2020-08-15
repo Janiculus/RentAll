3b773d4fc259f538ea38eb4b1bab9e91a3a5d150
wldstsk <osackij@gmail.com>
2020-08-15 21:43:31 +0300

    Added external product view

---
+++ Backend/spring-boot-rentall/src/main/java/com/cookie/rentall/views/ExternalProductView.java
@@ -0,0 +1,53 @@
+package com.cookie.rentall.views;
+
+public class ExternalProductView {
+    public ExternalProductView() {
+        name = "ExampleProduct";
+    }
+
+    private String type;
+    private String name;
+    private String category;
+    private String productID;
+    private Double price;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public void setCategory(String category) {
+        this.category = category;
+    }
+
+    public Double getPrice() {
+        return price;
+    }
+
+    public void setPrice(Double price) {
+        this.price = price;
+    }
+
+    public String getProductID() {
+        return productID;
+    }
+
+    public void setProductID(String productID) {
+        this.productID = productID;
+    }
+}
