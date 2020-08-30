e691b9438d7e795c4a8eddf96acb0fd9ecca7821
wldstsk <osackij@gmail.com>
2020-08-30 19:59:50 +0300

    Allowed CORS for external product controller

--- Backend/spring-boot-rentall/src/main/java/com/cookie/rentall/controllers/IntegrationController.java
+++ Backend/spring-boot-rentall/src/main/java/com/cookie/rentall/controllers/IntegrationController.java
@@ -3,11 +3,13 @@ package com.cookie.rentall.controllers;
 import com.cookie.rentall.services.ExternalProductService;
 import com.cookie.rentall.views.ExternalProductView;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
+@CrossOrigin
 @RestController
 public class IntegrationController {
 
---
+++ sajt/src/app/common/ExternalProduct.ts
@@ -0,0 +1,10 @@
+export class ExternalProduct {
+  name: string;
+  category: string;
+  productID: string;
+  pojemnoscSilnika: string;
+  markaSilnika: string;
+  szerokoscKoszenia: string;
+  regulacjaWysokosciKoszenia: number;
+  pojemnoscKosza: string;
+}
