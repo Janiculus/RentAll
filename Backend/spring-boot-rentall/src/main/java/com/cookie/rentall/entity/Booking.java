a1100f52d48b9c6627a6af4e5e3d2f372a67a3a5
Denys <novakden8@gmail.com>
2020-10-26 00:52:23 +0100

    Create class Booking with fields userId long, product Product @ManyToOne, pinCode int, createDate Date, bookingDate Date, returnDate Date

--- Backend/spring-boot-rentall/src/main/java/com/cookie/rentall/entity/Booking.java
+++ Backend/spring-boot-rentall/src/main/java/com/cookie/rentall/entity/Booking.java
@@ -7,27 +7,84 @@ import javax.persistence.*;
 import java.util.Date;
 
 @Entity
-@Table(name="booking")
+@Table(name = "booking")
 @Data
 public class Booking {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "id")
-    private long userId;
+    private long id;
 
     @ManyToOne
-    @JoinColumn(name="product_id", nullable = false)
+    @JoinColumn(name = "product_id", nullable = false)
     private Product product;
 
     @Column(name = "pin_code")
     private int pinCode;
+
     @Column(name = "create_date")
     @CreationTimestamp
     private Date createDate;
-    @Column(name= "booking_date")
+
+    @Column(name = "booking_date")
     private Date bookingDate;
-    @Column(name="return_date")
+
+    @Column(name = "return_date")
     private Date returnDate;
 
+    @Column(name = "user_id")
+    private long userId;
+
+    public long getId() {
+        return id;
+    }
+
+    public Product getProduct() {
+        return product;
+    }
+
+    public void setProduct(Product product) {
+        this.product = product;
+    }
+
+    public int getPinCode() {
+        return pinCode;
+    }
+
+    public void setPinCode(int pinCode) {
+        this.pinCode = pinCode;
+    }
+
+    public Date getCreateDate() {
+        return createDate;
+    }
+
+    public void setCreateDate(Date createDate) {
+        this.createDate = createDate;
+    }
+
+    public Date getBookingDate() {
+        return bookingDate;
+    }
+
+    public void setBookingDate(Date bookingDate) {
+        this.bookingDate = bookingDate;
+    }
+
+    public Date getReturnDate() {
+        return returnDate;
+    }
+
+    public void setReturnDate(Date returnDate) {
+        this.returnDate = returnDate;
+    }
+
+    public long getUserId() {
+        return userId;
+    }
+
+    public void setUserId(long userId) {
+        this.userId = userId;
+    }
 }
