3c2e00bd81cefa7fcb2a42c5ac986595fb0e4ed1
wldstsk <osackij@gmail.com>
2020-10-28 00:36:24 +0100

    free products section by owner

---
+++ sajt/src/app/owner-products/free/free.component.ts
@@ -0,0 +1,26 @@
+import { Component, OnInit } from '@angular/core';
+import {AppComponent} from "../../app.component";
+import {Product} from "../../common/product";
+import {ProductService} from "../../services/product.service";
+
+@Component({
+  selector: 'app-free',
+  templateUrl: './free.component.html',
+  styleUrls: ['./free.component.scss']
+})
+export class FreeComponent implements OnInit {
+  appComponent: AppComponent;
+  products: Product[] = [];
+
+  constructor(private productService: ProductService) { }
+
+  ngOnInit(): void {
+    this.appComponent = this.productService.getAppComponent();
+    this.productService.listProductsByOwner(1, 'RETURNED').subscribe(
+      data => {
+        this.products = data;
+      }
+    );
+  }
+
+}
