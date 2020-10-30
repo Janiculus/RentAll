52899b8961a169139a5c46623ca34a845a97cb7d
wldstsk <osackij@gmail.com>
2020-10-30 02:05:35 +0100

    upgrading reservation,booking and returning methods

--- sajt/src/app/services/product.service.ts
+++ sajt/src/app/services/product.service.ts
@@ -1,24 +1,26 @@
-import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
-import { Product } from '../common/product';
-// import { ExternalProduct } from '../common/ExternalProduct';
-import { Observable } from 'rxjs';
-import  { map } from 'rxjs/operators';
-import { ProductCategory } from '../common/product-category';
-import {ExternalProduct} from '../common/ExternalProduct';
+import {Injectable} from '@angular/core';
+import {HttpClient} from '@angular/common/http';
+import {Product} from '../common/product';
+//import { ExternalProduct } from '../common/ExternalProduct';
+import {Observable} from 'rxjs';
+import {map} from 'rxjs/operators';
+import {ProductCategory} from '../common/product-category';
+import {ExternalProduct} from "../common/ExternalProduct";
+import {AppComponent} from "../app.component";
+import {ProductStatus} from "../common/productstatus";
 
 @Injectable({
   providedIn: 'root'
 })
 export class ProductService {
-
-
+  private appComponent: AppComponent;
 
   private baseUrl = 'http://localhost:8080/api/products';
 
   private categoryUrl = 'http://localhost:8080/api/product_category';
 
-  constructor(private httpClient: HttpClient) { }
+  constructor(private httpClient: HttpClient) {
+  }
 
   getProduct(theProductId: number): Observable<Product> {
 
@@ -28,14 +30,29 @@ export class ProductService {
     return this.httpClient.get<Product>(productUrl);
   }
 
+  getProductStatus(theProductId: number): Observable<ProductStatus> {
+    const productUrl = `${this.baseUrl}/${theProductId}/status`;
+    return this.httpClient.get<ProductStatus>(productUrl);
+  }
+
+  getProductConsumer(theProductId: number): Observable<number> {
+    const productUrl = `${this.baseUrl}/${theProductId}/consumer`;
+    return this.httpClient.get<number>(productUrl);
+  }
+
+  getCurrentUser(): Observable<number> {
+    const productUrl = `${this.baseUrl}/currentUser`;
+    return this.httpClient.get<number>(productUrl);
+  }
+
   getProductListPaginate(thePage: number,
                          thePageSize: number,
                          theCategoryId: number): Observable<GetResponseProducts> {
 
 
-                          // need to build URL based on category id, page and size
+    // need to build URL based on category id, page and size
     const searchUrl = `${this.baseUrl}/search/findByCategoryId?id=${theCategoryId}`
-                      + `&page=${thePage}&size=${thePageSize}`;
+      + `&page=${thePage}&size=${thePageSize}`;
 
     return this.httpClient.get<GetResponseProducts>(searchUrl);
   }
@@ -67,58 +84,60 @@ export class ProductService {
   searchProducts(theKeyword: string): Observable<Product[]> {
 
     // need to build URL based on the keyword
-    const searchUrl = `${this.baseUrl}/search/findByNameContaining?name=${theKeyword}`;
+    /*const searchUrl = `${this.baseUrl}/search/findByNameContaining?name=${theKeyword}`;
 
-    return this.getProducts(searchUrl);
+    return this.getProducts(searchUrl);*/
+    return this.httpClient.get<GetResponseProductsPlain>(`${this.baseUrl}/available`).pipe(map(response => response.content));
 
   }
+
 // RESERVE PRODUCT
-  reserveProduct(theProductId: number): Observable<any> {
-    const reserveUrl = `${this.baseUrl}/${theProductId}`;
-    const headers = {'content-type' : 'application/json'};
-    const body = {active : 'false'};
+  reserveProduct(theProductId: string): Observable<any> {
+    const reserveUrl = `${this.baseUrl}/${theProductId}/book`;
+    const headers = {'content-type': 'application/json'};
+    const body = {active: 'false'};
     return this.httpClient.patch(reserveUrl, body, {headers: headers});
   }
+
+// CANCEL RESERVATION
+  cancelReservation(theProductId: string): Observable<any> {
+    const reserveUrl = `${this.baseUrl}/${theProductId}/cancel`;
+    const headers = {'content-type': 'application/json'};
+    const body = {active: 'false'};
+    return this.httpClient.patch(reserveUrl, body, {headers: headers});
+  }
+
+  // BOOK PRODUCT
+  bookProduct(theProductId: string): Observable<any> {
+    const bookUrl = `${this.baseUrl}/${theProductId}/get`;
+    const headers = {'content-type': 'application/json'};
+    const body = {active: 'false'};
+    return this.httpClient.patch(bookUrl, body, {headers: headers});
+  }
+
 // RETURN PRODUCT
-  returnProduct(theProductId: number): Observable<any> {
-    const returnUrl = `${this.baseUrl}/${theProductId}`;
-    const headers = {'content-type' : 'application/json'};
-    const body = {active : 'true'};
+  returnProduct(theProductId: string): Observable<any> {
+    const returnUrl = `${this.baseUrl}/${theProductId}/return`;
+    const headers = {'content-type': 'application/json'};
+    const body = {active: 'true'};
     return this.httpClient.patch(returnUrl, body, {headers: headers});
   }
 
-  listProductsByOwner(userId: number, status: boolean): Observable<Product[]> {
-
-    const searchUrl = `${this.baseUrl}/createdByUser/findByUserId?id=${userId}`;
-
-    return this.getProducts(searchUrl);
+  listProductsByOwner(userId: number, status: string): Observable<Product[]> {
+    const url = `${this.baseUrl}/createdByUser?status=${status}`;
+    return this.httpClient.get<GetResponseProductsPlain>(url).pipe(map(response => response.content));
   }
 
-  listProductsByConsumer(userId: number, status: boolean): Observable<Product[]> {
-
-    const searchUrl = `${this.baseUrl}/gotByUser/findByUserId?id=${userId}`;
-
-    return this.getProducts(searchUrl);
+  listProductsByConsumer(userId: number, status: string): Observable<Product[]> {
+    const url = `${this.baseUrl}/gotByUser?status=${status}`;
+    return this.httpClient.get<GetResponseProductsPlain>(url).pipe(map(response => response.content));
   }
 
   addProduct(product: Product): Observable<any> {
-    const headers = {'content-type': 'application/json'};
+    const headers = {'content-type': 'application/json'}
     const body = JSON.stringify(product);
     console.log(body);
-    return this.httpClient.post(this.baseUrl, body, {headers: headers});
-  }
-
-  deleteProduct(theProductId: number): Observable<Product> {
-
-    const productUrl = `${this.baseUrl}/${theProductId}`;
-    return this.httpClient.delete<Product>(productUrl);
-  }
-
-  updateProduct(product: Product): Observable<any> {
-    const headers = {'content-type': 'application/json'};
-    const body = JSON.stringify(product);
-    console.log(body);
-    return this.httpClient.put(this.baseUrl, body, {headers: headers});
+    return this.httpClient.post(this.baseUrl, body, {'headers': headers});
   }
 
   searchCityProducts(cityName: string): Observable<Product[]> {
@@ -127,26 +146,53 @@ export class ProductService {
     return this.getProducts(searchUrl);
   }
 
+  setAppComponent(ac: AppComponent) {
+    this.appComponent = ac;
+  }
+
+  getAppComponent(): AppComponent {
+    return this.appComponent;
+  }
 }
 
 interface GetResponseProducts {
   _embedded: {
     products: Product[];
-  };
+  },
   page: {
     size: number,
     totalElements: number,
     totalPages: number,
     number: number
-  };
+  }
+}
+
+interface Sort {
+  sorted: boolean;
+  unsorted: boolean;
+  empty: boolean;
+}
+
+interface GetResponseProductsPlain {
+  content: Product[];
+  pageable: string;
+  last: boolean;
+  totalPages: bigint;
+  totalElements: bigint;
+  size: bigint;
+  number: bigint;
+  sort: Sort;
+  numberOfElements: bigint;
+  first: boolean;
+  empty: boolean
 }
 
 interface GetResponseProductCategory {
   _embedded: {
     productCategory: ProductCategory[];
-  };
+  }
 }
 
 interface GetResponseExternalProducts {
-    products: ExternalProduct[];
+  products: ExternalProduct[];
 }
